bb119ae291dceed8959e92dedb9ed9e9f7bff913
jung <jeong00015@gmail.com>
2022-12-06 21:31:59 +0900

    test 3

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어줍니다.
@@ -11,5 +13,10 @@ public class HelloController {
     @GetMapping(value = "/hello") // 예전에는 RequsetMapping(method = RequsetMethod.GET)으로 사용되었습니다.
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping(value = "/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name , @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name , amount);
     }
 }
--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,7 +1,26 @@
 package com.jojoldu.book.springboot.web.dto;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.Assert.*;
 
 public class HelloResponseDtoTest {
 
+    @Test
+    @DisplayName("롬복 기능 테스트")
+    public void 롬복_기능_테스트() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name , amount);
+
+        // then
+        assertEquals(helloResponseDto.getName() , name);
+        assertEquals(helloResponseDto.getAmount() , amount);
+
+
+    }
 }
